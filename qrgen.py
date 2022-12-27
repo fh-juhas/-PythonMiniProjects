ee0e181945c7e91e9cf75c2d79f65c95039e04f1
fh-juhas <81632475+fh-juhas@users.noreply.github.com>
2022-12-27 20:17:53 +0600

    Update qrgen.py
    3rd commit

--- qrgen.py
+++ qrgen.py
@@ -2,8 +2,8 @@ import pyqrcode
 from PIL import Image, ImageDraw
 import png
 
-data = 'https://www.facebook.com/fhjuhas.cse'
+data = input("Enter data to convert into qrcode: ")
 img = pyqrcode.create(data)
 #img.save('C:/Users/fhjuh/OneDrive/Pictures/qr/fhj.png')
 #print(img.terminal(quiet_zone=1))
-img.png("fhj_fb.png",scale=8)
+img.png("data.png",scale=8)
